e37417b56f0c4810553337b8ce5463bd7d1863f5
kaji <dafe00842202@outlook.jp>
2026-07-17 01:23:10 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://kaji.blog',
+  // site: 'https://kaji.blog',
+  site: 'https://ryokeeeesh.github.io/',
+  base: '/portfolio',
 });
