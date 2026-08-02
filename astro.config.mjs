0e04810678f34e12f15968180a1c85968d5df246
kaji <dafe00842202@outlook.jp>
2026-08-03 01:24:34 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,50 @@
+import { unified } from '@astrojs/markdown-remark';
 import sitemap from '@astrojs/sitemap';
+import { pluginFileIcons } from '@xt0rted/expressive-code-file-icons'; // https://github.com/xt0rted/expressive-code-file-icons
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code'; // https://expressive-code.com/key-features/syntax-highlighting/
+import { pluginColorChips } from 'expressive-code-color-chips'; // https://delucis.github.io/expressive-code-color-chips/getting-started/
 import rehypeMermaid from 'rehype-mermaid';
 import remarkBehead from 'remark-behead';
+
+// https://twoslash.studiocms.dev/
+// 今後あんま使いそうになかったら消すかも
 
 export default defineConfig({
   site: 'https://kaji.blog',
   trailingSlash: 'never',
-  integrations: [sitemap()],
-
-  markdown: {
-    remarkPlugins: [
-      [remarkBehead, { depth: 2 }]
-    ],
-    syntaxHighlight: {
-      type: 'shiki',
-      excludeLangs: ['mermaid'],
-    },
-    rehypePlugins: [
-      [
-        rehypeMermaid,
-        {
-          strategy: 'inline-svg',
-          mermaidOptions: {
-            theme: 'neutral',
-          },
-        },
+  integrations: [
+    sitemap(),
+    expressiveCode({
+      themes: ['github-dark', 'github-light'],
+      plugins: [
+        pluginFileIcons({
+          iconClass: 'code-icon',
+          titleClass: 'code-title',
+        }),
+        pluginColorChips(),
       ],
-    ],
+      styleOverrides: {
+        colorChips: {
+          borderRadius: 0,
+        },
+      },
+    }),
+  ],
+  markdown: {
+    processor: unified({
+      remarkPlugins: [[remarkBehead, { depth: 2 }]],
+      rehypePlugins: [
+        [
+          rehypeMermaid,
+          {
+            strategy: 'inline-svg',
+            mermaidOptions: {
+              theme: 'neutral',
+            },
+          },
+        ],
+      ],
+    }),
   },
 });
--- src/posts/test-blog.md
+++ src/posts/test-blog.md
@@ -18,3 +18,23 @@ issueNumber: 1
 ###### テスト本文
 
 ここに書いた文字が画面に表示されるか実験します！
+
+```ts title="example.ts"
+console.log("banana")
+```
+
+```js title="example.js" {7} del={3} ins={4} "str"
+const main = (str) => {
+  console.log(str)
+  console.log("apple")
+  console.log("banana")
+}
+
+main("Hello! I like...")
+```
+
+```css title="global.css"
+.example {
+  color: goldenrod;
+}
+```
